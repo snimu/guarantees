bae5f18ffaa537de8210455792ae1f897835f563
snimu <sebi.mueller55@gmail.com>
2022-08-21 15:00:52 +0200

    rename test files, change TODOs

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -2,21 +2,21 @@ import unittest
 
 # Import unittest from their respective files
 #   -> unittest.main() will run them all
-from tests._test_types_numeric import TestNumericGuarantee
-from tests._test_types_string import TestStringGuarantee
-from tests._test_types_boolean import TestBooleanGuarantee
-from tests._test_types_other import TestIsClass, TestIsNone, TestIsUnion
-from tests._test_types_collections import TestCollectionsGuarantee
+from tests._test_parameters_numeric import TestNumericGuarantee
+from tests._test_parameters_string import TestStringGuarantee
+from tests._test_parameters_boolean import TestBooleanGuarantee
+from tests._test_parameters_other import TestIsClass, TestIsNone, TestIsUnion
+from tests._test_parameters_collections import TestCollectionsGuarantee
 from tests._test_parameters_common import TestCallback, test_onoff
 from tests._test_parameters_binary import TestBinary
-
-# TODO (snimu) implement enforcement & tests for binary types
 
 # TODO (snimu) "description" parameter?
 
 # TODO (snimu) Write docstrings
 
 # TODO (snimu) classes for return values
+
+# TODO (snimu) guarantee tests
 
 # TODO (snimu) classes for other languages: Rust, C++, Java, etc.
 #   -> one branch per language.

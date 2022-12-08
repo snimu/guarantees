69bc93f8eeef1da3a43cb587a95cf041795e57bf
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-08 18:04:41 +0100

    fix another small error in error-strings for type-exceptions.

--- pyguarantees/functional_guarantees/classes/util/common_checks.py
+++ pyguarantees/functional_guarantees/classes/util/common_checks.py
@@ -22,7 +22,7 @@ def check_type(arg, guarantee):
         guarantee=guarantee,
         parameter_name=guarantee.parameter_name,
         what_dict={
-            "should_type": get_arg_type_name(guarantee.guaranteed_type),
+            "should_type": guarantee.guaranteed_type,
             "actual_type": get_arg_type_name(arg),
             "force_conversion": guarantee.force_conversion
         }

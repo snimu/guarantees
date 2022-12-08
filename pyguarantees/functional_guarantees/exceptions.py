6e7f9c703caf38782a02e21783652164da83cb09
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-08 17:49:37 +0100

    consolidate files from `exceptions`-dir into one `exceptions.py`-file for clarity

--- pyguarantees/functional_guarantees/exceptions.py
+++ pyguarantees/functional_guarantees/exceptions.py
@@ -1,5 +1,41 @@
-from ._util import construct_err_str
 from pyguarantees import severity
+
+
+def _parse_what_dict(what_dict: dict, tabs: str = "\t") -> str:
+    err_str = ""
+    for key, val in what_dict.items():
+        if type(val) is dict:
+            err_str += f"{tabs}key: \n"
+            err_str += _parse_what_dict(val, tabs + "\t")
+        else:
+            err_str += f"{tabs}{str(key)}: {str(val)} \n"
+
+    return err_str
+
+
+def construct_err_str(
+        qualname: str,
+        module: str,
+        guarantee_name: str,
+        parameter_name: str,
+        error_severity: int,
+        what_dict: dict = None
+) -> str:
+    severity_str_dict = {
+        severity.DEBUG: "DEBUG",
+        severity.INFO: "INFO",
+        severity.WARN: "WARN",
+        severity.ERROR: "ERROR",
+        severity.FATAL: "FATAL"
+    }
+    err_str = f"\n\tWhere: \n"
+    err_str += f"\t\tModule: {module} \n"
+    err_str += f"\t\tName: {qualname} \n"
+    err_str += f"\tSeverity: {severity_str_dict[error_severity]} \n"
+    err_str += f"\tGuarantee: {guarantee_name} \n"
+    err_str += f"\tParameter: {parameter_name} \n"
+    err_str += _parse_what_dict(what_dict)
+    return err_str
 
 
 class ParameterGuaranteesValueError(ValueError):
--- pyguarantees/functional_guarantees/exceptions/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._exceptions import \
-    ParameterGuaranteesTypeError, ParameterGuaranteesValueError, \
-    ReturnGuaranteesTypeError, ReturnGuaranteesValueError, \
-    FunctionalGuaranteesUserTypeError, FunctionalGuaranteesUserValueError
-from ._util import construct_err_str

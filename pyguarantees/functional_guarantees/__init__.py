f6134354e2c5d3fe1126fcbb73a6e78f26db684a
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-13 11:56:27 +0100

    move common checks for collections into `fg.classes.util.collections_checks.py`

--- pyguarantees/functional_guarantees/__init__.py
+++ pyguarantees/functional_guarantees/__init__.py
@@ -1,7 +1,7 @@
 from .classes import IsBool, IsStr, IsBytes, IsByteArray, IsSet, IsFrozenSet,\
     IsList, IsDict, IsTuple, IsRange, IsClass, IsFloat, IsInt, IsMemoryView, \
     IsComplex, Guarantee, NumericGuarantee, TypeGuarantee, NoOp, IsNone, IsUnion, \
-    DynamicCheck
+    DynamicCheck, CollectionType
 from .decorator import add_guarantees, settings
 import pyguarantees.functional_guarantees.exceptions
 import pyguarantees.functional_guarantees.decorator.settings

0051088318855635183cde96333cc66638cf2568
sebastianmuller <sebastian.nicolas.mueller@gmail.com>
2022-12-17 16:15:59 +0100

    find way to test `NotUsedInTestsError`

--- tests/test_guarantees/test_exceptions.py
+++ tests/test_guarantees/test_exceptions.py
@@ -18,4 +18,25 @@ def test_TestsNotImplementedError():
 
 @pytest.mark.order(1)
 def test_NotUsedInTestsError():
-    pass   # TODO
+    @tg.guarantee_test()
+    @tg.guarantee_usage()
+    def fct(a):
+        return a
+
+    @tg.implements_test_for(fct)
+    def random_function():
+        pass
+
+    random_function()
+
+    with pytest.raises(tg.exceptions.NotUsedInTestsError):
+        tg.test_functions_used_in_tests()
+
+    # test_functions_used_in_tests will be called at the end by pytest;
+    #   make sure that it won't err
+    _mark_as_used(fct)
+
+
+def _mark_as_used(fct):
+    tg._decorators.fdata[fct]["testcases_without_exec"] = None
+
